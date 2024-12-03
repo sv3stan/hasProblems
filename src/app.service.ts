956302c82800bc0e68b0a6f038f2f0686bb0b415
sv3stan <tristan.sergey@gmail.com>
2024-12-03 13:37:22 +0300

    added problem.dump

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Test task for working with a large amount of data in a postgres database!';
   }
 }
